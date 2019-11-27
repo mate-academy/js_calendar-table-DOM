d558a71aedf4c0f1fef644acb0090822d605df2d
Anna Gerdii <annagerdiy@gmail.com>
2019-11-27 17:25:29 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,43 @@
 
 const calendar = document.querySelector('#calendar');
 
+const getLocalDay = date => {
+  let day = date.getDay();
+
+  if (day === 0) {
+    day = 7;
+  }
+
+  return day;
+};
+
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const monthLength = new Date(year, month, 0).getDate();
+  const monthStartDay = getLocalDay(new Date(year, month - 1));
+  const monthEndDay = getLocalDay(new Date(year, month - 1, monthLength));
+
+  let htmlCode = '';
+
+  htmlCode += `<tr><th>MO</th><th>TU</th><th>WE</th><th>TH</th>
+  <th>FR</th><th>SA</th><th>SU</th></tr><tr>`;
+
+  for (let i = 1; i < monthStartDay; i++) {
+    htmlCode += `<td></td>`;
+  }
+
+  for (let i = 1; i <= monthLength; i++) {
+    if ((i + monthStartDay - 1) % 7 === 0) {
+      htmlCode += `<td>${i}</td></tr><tr>`;
+    } else {
+      htmlCode += `<td>${i}</td>`;
+    }
+  }
+
+  for (let i = 1; i <= 7 - monthEndDay; i++) {
+    htmlCode += `<td></td>`;
+  }
+
+  element.innerHTML = `<table> ${htmlCode} </tr></table>`;
 }
 
-calendarTable(2019, 10, calendar);
+calendarTable(2019, 11, calendar);
