41b05ba9fd64ecbd7bd16f7be2d7e6df657ab2e1
Denys <denys.cheporniuk@gmail.com>
2019-11-27 15:07:22 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,32 @@
+/* eslint-disable indent */
 'use strict';
 
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const firstDay = new Date(year, month, 0).getDay();
+  const numberOfDays = Array(new Date(year, month + 1, 0).getDate()).fill('')
+                       .map((item, index) => index + 1);
+  const days = ['ПН', 'ВТ', 'СР', 'ЧТ', 'ПТ', 'СБ', 'НД'];
+
+  element.innerHTML = `
+    <table>
+        <tr>
+          ${days.map(day => `<th>${day}</th>`).join('')}
+        </tr>
+        <tr>
+          ${Array(firstDay).fill('').map(item => `<td></td>`).join('')}
+          ${numberOfDays.map((day, index) => {
+            if ((firstDay + index) % 7 === 0) {
+              return `<tr><td>${day}</td>`;
+            };
+
+            return `<td>${day}</td>`;
+          }).join('')}
+    </table>
+  `;
+
+  return element;
 }
 
 calendarTable(2019, 10, calendar);
