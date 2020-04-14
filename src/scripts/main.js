bd9cd20b111ab182397a4058b349ed931bd7438f
Rustam Khananov <antmine2017@gmail.com>
2020-04-14 15:24:46 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,45 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const date = new Date(year, month - 1);
+  let weekDay = date.getDay();
+  const daysInMonth = new Date(year, month, 0).getDate();
+  let daysCount = 1;
+  const table = document.createElement('table');
+  let week = '';
+  let monthStr = '';
+  const thead = `<thead>
+                <tr>
+                <td>ПН</td>
+                <td>ВТ</td>
+                <td>СР</td>
+                <td>ЧТ</td>
+                <td>ПТ</td>
+                <td>СБ</td>
+                <td>ВС</td>
+                </tr>
+                </thead>`;
+
+  for (let weekCount = 1; weekCount < 6; weekCount++) {
+    for (let i = 1; i < 8; i++) {
+      if (weekDay > i || daysCount > daysInMonth) {
+        week += `<td></td>`;
+      } else {
+        week += `<td>${daysCount}</td>`;
+        daysCount++;
+      };
+    };
+    weekDay = 1;
+    monthStr += `<tr>${week}</tr>`;
+    week = '';
+  };
+
+  table.innerHTML = `${thead}<tbody>${monthStr}</tbody>`;
+  table.children[0].style.color = 'white';
+  table.children[0].style.backgroundColor = 'red';
+  table.children[1].style.color = 'teal';
+  table.children[1].style.fontSize = '24px';
+  element.append(table);
 }
 
 calendarTable(2019, 10, calendar);
