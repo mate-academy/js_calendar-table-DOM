e8989a69f3a385d6c064075a0612d98339716512
Oleg <fasfas7971@gmail.com>
2020-04-14 14:33:20 +0300

    Fix hidden day

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,11 +3,17 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  const firstDay = new Date(year, month - 1).getDay() - 1;
+  const dayInWeek = 7;
+  let firstDay = new Date(year, month - 1).getDay() - 1;
   const daysInMonth = new Date(year, month, 0).getDate();
-  const weeksCounter = Math.ceil(daysInMonth / 7);
+  let weeksCounter = Math.ceil(daysInMonth / dayInWeek);
   const daysName = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
   const isPresentDay = [];
+
+  if (firstDay < 0) {
+    weeksCounter = weeksCounter + 1;
+    firstDay = firstDay + dayInWeek;
+  }
 
   for (let i = 0; i < weeksCounter * 7; i++) {
     const thisDay = i - firstDay + 1;
@@ -15,7 +21,7 @@ function calendarTable(year, month, element) {
     if (thisDay > 0 && thisDay <= daysInMonth) {
       isPresentDay.push(`<td class="active__td">${thisDay}</td>`);
     } else {
-      isPresentDay.push(`<td></td>`);
+      isPresentDay.push(`<td class="non-active__td"></td>`);
     }
   }
 
@@ -39,4 +45,4 @@ function calendarTable(year, month, element) {
   `;
 }
 
-calendarTable(2020, 4, calendar);
+calendarTable(2020, 3, calendar);
