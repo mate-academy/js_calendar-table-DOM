a80901b22c65976d869671578f76456ae74c38c3
Vitalii Shytikov <v.shytikov@gmail.com>
2020-04-14 20:48:04 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,54 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const daysInMonth = new Date(year, month, 0).getDate();
+  const startDay = new Date(`${year}, ${month}`).getDay();
+  const weeks = Math.ceil((daysInMonth + startDay) / 7);
+  const days = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+
+  let tHead = '';
+  let tBody = '';
+  let dateCounter = 1;
+
+  for (let i = 0; i < days.length; i++) {
+    tHead += `
+      <th>${days[i]}</th>
+    `;
+  }
+
+  for (let row = 0; row < weeks; row++) {
+    tBody += `
+      <tr>
+    `;
+
+    for (let day = 0; day < days.length; day++) {
+      if (row === 0 && day < startDay) {
+        tBody += `
+          <td></td>
+        `;
+      } else if (dateCounter <= daysInMonth) {
+        tBody += `
+          <td>${dateCounter}</td>
+        `;
+        dateCounter++;
+      } else {
+        tBody += `
+          <td></td>
+        `;
+      }
+    }
+  }
+
+  element.innerHTML = `
+    <table>
+      <thead>
+        ${tHead}
+      </thead>
+      <tbody>
+        ${tBody}
+      </tbody>
+    </table>
+  `;
 }
 
-calendarTable(2019, 10, calendar);
+calendarTable(2019, 6, calendar);
