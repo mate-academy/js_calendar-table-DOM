5183db0726a4af4885fd226dab6295d80cb096a0
Andrey Faychak <faychak333@gmail.com>
2020-04-15 18:58:17 +0300

    'ok'

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,23 +2,11 @@
 
 const calendar = document.querySelector('#calendar');
 
-function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+function calendarTable(year, month) {
   const days = Math.round((new Date(year, month)
     - new Date(year, month - 1))
     / 24 / 3600 / 1000);
-  const table = document.createElement('table');
-  const thead = document.createElement('thead');
-  const tbody = document.createElement('tbody');
-  let tr = document.createElement('tr');
   const nameOfDays = {
-    1: 'пн',
-    2: 'вт',
-    3: 'ср',
-    4: 'чт',
-    5: 'пт',
-    6: 'сб',
-    7: 'вс',
     'Mon': 1,
     'Tus': 2,
     'Wed': 3,
@@ -27,40 +15,45 @@ function calendarTable(year, month, element) {
     'Sat': 6,
     'Sun': 7,
   };
+  const daysOfWeek = [
+    'пн',
+    'вт',
+    'ср',
+    'чт',
+    'пт',
+    'сб',
+    'нд',
+  ];
   const day = nameOfDays[new Date(year, month - 1).toString().split(' ')[0]];
   let count = 2 - day;
-
-  element.append(table);
-  table.append(thead);
-  table.append(tbody);
-  thead.append(tr);
-  table.className = 'table';
-  tr.className = 'tr';
-
-  for (let i = 1; i <= 7; i++) {
-    const th = document.createElement('th');
-
-    th.className = 'th';
-    th.textContent = nameOfDays[i];
-    tr.append(th);
-  }
+  const arrWeeks = [];
 
   for (let i = 0; i < (day + days - 1) / 7; i++) {
-    tr = document.createElement('tr');
-    tr.className = 'tr';
-    tbody.append(tr);
+    const myArr = [];
 
     for (let j = 0; j < 7; j++) {
-      const td = document.createElement('td');
-
       if (count <= days && count >= 1) {
-        td.textContent = count;
+        myArr.push(`<td>${count}</td>`);
+      } else {
+        myArr.push(`<td></td>`);
       }
       count++;
-      td.className = 'td)';
-      tr.append(td);
     }
+    arrWeeks.push(`<tr>${myArr.join('')}</tr>`);
   }
+
+  calendar.innerHTML = `
+    <table>
+      <thead>
+        <tr>
+          ${daysOfWeek.map(el => `<th>${el}</th>`).join('')}
+        </tr>
+      </thead>
+      <tbody>
+        ${arrWeeks.join('')}
+      </tbody>
+    </table>
+  `;
 }
 
 calendarTable(2020, 3, calendar);
