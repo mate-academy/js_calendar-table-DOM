6b0d5f492112631f1aef71cfd510b904c7337ad2
Tata2222 <tataolcha@gmail.com>
2020-04-15 11:39:48 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,52 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const date = new Date(year, month - 1, 1);
+  const firstDay = date.getDay();
+  const days = new Date(year, month - 1, 0).getDate();
+  const table = document.createElement('table');
+  const thead = document.createElement('thead');
+  const tbody = document.createElement('tbody');
+  const weekDays = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  let number = 7 - (firstDay - 1) + 1;
+  let firstWeekNumber = 1;
+
+  table.append(thead);
+  table.append(tbody);
+
+  for (let i = 0; i < 6; i++) {
+    const tr = document.createElement('tr');
+
+    if (i === 0) {
+      thead.append(tr);
+    } else {
+      tbody.append(tr);
+    }
+
+    for (let j = 0; j < 7; j++) {
+      const th = document.createElement('th');
+      const td = document.createElement('td');
+
+      if (i === 0) {
+        th.innerHTML = weekDays[j];
+        tr.append(th);
+      } else {
+        if ((i === 1) && (j >= firstDay - 1)) {
+          td.innerHTML = firstWeekNumber;
+          firstWeekNumber++;
+        }
+
+        if ((i >= 2) && (number <= days)) {
+          td.innerHTML = number;
+          number++;
+        }
+
+        tr.append(td);
+      }
+    }
+  }
+
+  calendar.append(table);
 }
 
 calendarTable(2019, 10, calendar);
