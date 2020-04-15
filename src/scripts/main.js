6020a9a03bddcc237a7ff71b3da44778675bd281
Andreas_Just <andreas.wq@gmail.com>
2020-04-15 18:10:09 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,49 +1,48 @@
 'use strict';
 
+const WEEK_DAYS = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+const WEEK_LENGTH = 7;
 const calendar = document.querySelector('#calendar');
+const options = {
+  year: 'numeric', month: 'long',
+};
 
 function calendarTable(year, month, element) {
   const date = new Date(year, month - 1);
+  const numDayInMonth = new Date(year, month, 0).getDate();
+  const starWeekDay = date.getDay() || 7;
+  const endWeekDay = new Date(year, month).getDay();
+  const strDate = date.toLocaleDateString('en-US', options);
+  const heading = `<caption>${strDate}</caption>`;
   let head = '';
   let body = '';
-  const WEEK_DAYS = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
-  let day = date.getDay();
 
   WEEK_DAYS.forEach(weekday => {
     head += `<th>${weekday}</th>`;
   });
 
-  if (day === 0) {
-    day = 7;
-  }
-
-  for (let i = 0; i < day - 1; i++) {
+  for (let i = 0; i < starWeekDay - 1; i++) {
     body += `<td></td>`;
   }
 
-  while (date.getMonth() === month - 1) {
-    body += `<td>${date.getDate()}</td>`;
+  for (let currDay = 0; currDay < numDayInMonth; currDay++) {
+    body += `<td>${currDay + 1}</td>`;
 
-    if (date.getDay() % 7 === 0) {
+    if ((currDay + starWeekDay) % 7 === 0) {
       body += `</tr><tr>`;
     }
-
-    day = date.getDay();
-    date.setDate(date.getDate() + 1);
   }
 
-  if (day !== 0) {
-    for (let i = date.getDay(); i <= 7; i++) {
-      body += `<td></td>`;
-    }
+  for (let i = endWeekDay; i <= WEEK_LENGTH; i++) {
+    body += `<td></td>`;
   }
 
   head = `<thead><tr>${head}</tr></thead>`;
   body = `<tbody><tr>${body}</tr></tbody>`;
 
-  const table = head + body;
+  const table = heading + head + body;
 
   element.innerHTML = `<table>${table}</table>`;
 }
 
-calendarTable(2020, 4, calendar);
+calendarTable(2020, 8, calendar);
