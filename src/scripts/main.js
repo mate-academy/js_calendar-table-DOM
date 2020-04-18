0b231062e03ad3d4defb23f752b4a2f529f87351
Kira Marks <serafita22@gmail.com>
2020-04-18 17:24:15 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,47 @@
 
 const calendar = document.querySelector('#calendar');
 
-function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+function calendarTable(year, humanMonth, element) {
+  const daysOfWeek = ['пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс'];
+  const month = humanMonth - 1;
+  const currentDate = new Date(year, month, 1);
+  const nextMonth = new Date(year, month + 1, 1);
+
+  while (currentDate.getDay() !== 1) {
+    currentDate.setDate(currentDate.getDate() - 1);
+  }
+
+  const weeks = [];
+
+  while (currentDate < nextMonth) {
+    const week = [];
+
+    for (let i = 0; i < 7; i++) {
+      if (currentDate.getMonth() !== month) {
+        week.push(0);
+      } else {
+        week.push(currentDate.getDate());
+      }
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+
+    weeks.push(week);
+  }
+
+  element.innerHTML = `
+      <table>
+      <thead>
+        <tr>
+        ${daysOfWeek.map(day => `<th>${day}</th>`).join('')}
+        </tr>
+      </thead>
+      <tbody>
+      ${weeks.map(week =>
+    `<tr>
+        ${week.map(day => `<td>${day || ''}</td>`).join('')}
+     </tr>`).join('')}
+      </tbody>
+    </table>`;
 }
 
 calendarTable(2019, 10, calendar);
