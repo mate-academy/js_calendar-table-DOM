913601fbcab3a78dfa9c14a86ef89cc25dca1bf2
Illia Oliynik <illiaoliynik@gmail.com>
2020-04-19 16:52:39 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,47 @@
 'use strict';
 
-const calendar = document.querySelector('#calendar');
+function calendarTable1(year, humanMonth, element) {
+  const daysInWeek = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+  const weeks = [];
+  const month = humanMonth - 1;
+  const dayInNextMonth = new Date(year, humanMonth, 1);
+  const currentDate = new Date(year, month, 1);
 
-function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  while (currentDate.getDay() !== 1) {
+    currentDate.setDate(currentDate.getDate() - 1);
+  }
+
+  // eslint-disable-next-line no-unmodified-loop-condition
+  while (currentDate < dayInNextMonth) {
+    const week = [];
+
+    for (let j = 0; j < 7; j++) {
+      if (currentDate.getMonth() !== month) {
+        week.push(0);
+      } else {
+        week.push(currentDate.getDate());
+      }
+      currentDate.setDate(currentDate.getDate() + 1);
+    }
+    weeks.push(week);
+  }
+
+  element.innerHTML = `
+<h1>Calendar ${year}-${humanMonth}</h1>
+<table>
+    <thead>
+        <tr>
+        ${daysInWeek.map(day => `<th>${day}</th>`).join('\n')}
+        </tr>
+    </thead>
+    <tbody>
+        ${weeks.map(week => `
+        <tr>
+            ${week.map(day => `
+            <td>${day || ''}</td>`).join('')}
+        </tr>`).join('\n')}
+    </tbody>
+</table>`;
 }
 
-calendarTable(2019, 10, calendar);
+calendarTable1(2020, 4, document.getElementById('calendar'));
