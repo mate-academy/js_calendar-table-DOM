606f6ec2e2a54687b2e8ab74e3a6f4305be3acf4
Campari229 <KennyKoin@gmail.com>
2020-06-16 14:49:51 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,44 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const weekDays = ['Mn', 'Tu', 'Wd', 'Th', 'Fr', 'St', 'Sn'];
+
+  const table = document.createElement('table');
+  const tableHead = document.createElement('thead');
+  const tableBody = document.createElement('tbody');
+
+  element.append(table);
+  table.append(tableHead);
+  table.append(tableBody);
+
+  for (let i = 0; i < 7; i++) {
+    const headCell = document.createElement('th');
+
+    headCell.innerText = weekDays[i];
+    tableHead.append(headCell);
+  }
+
+  const daysInMonth = new Date(year, month, 0).getDate();
+  const monthStart = new Date(year, month - 1, 1).getDay();
+  const rowCOunt = Math.ceil(daysInMonth / 7);
+
+  for (let i = 0; i < rowCOunt; i++) {
+    const tableRow = document.createElement('tr');
+
+    tableBody.append(tableRow);
+
+    for (let j = 1; j <= 7; j++) {
+      const bodyCell = document.createElement('td');
+
+      if (j + (i * 7) < monthStart || j + (i * 7) - monthStart >= daysInMonth) {
+        bodyCell.innerText = '';
+      } else {
+        bodyCell.innerText = j + (i * 7) - monthStart + 1;
+      }
+
+      tableRow.append(bodyCell);
+    }
+  }
 }
 
-calendarTable(2019, 10, calendar);
+calendarTable(2013, 8, calendar);
