9503b881aaea13b04e714f7a779a69b0f618c5d6
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-06-17 00:55:03 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,6 +4,42 @@ const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
   // WRITE YOUR CODE HERE
+  const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
+  const firstWeekDay = new Date(year, month, 1).getDay();
+  const lastMonthDay = new Date(year, month + 1, 0).getDate();
+  const calendarRows = (firstWeekDay + lastMonthDay) / 7;
+  const table = document.createElement('table');
+  const tbody = document.createElement('tbody');
+  const thead = table.createTHead();
+  const headRow = thead.insertRow();
+
+  element.append(table);
+  table.append(thead);
+  table.append(tbody);
+
+  for (const day of weekDays) {
+    const th = document.createElement('th');
+
+    headRow.appendChild(th);
+    th.textContent = day;
+  }
+
+  let dateDay = 1;
+
+  for (let i = 0; i < calendarRows; i++) {
+    const tr = tbody.insertRow();
+
+    for (let j = 0; j < 7; j++) {
+      const td = document.createElement('td');
+
+      tr.append(td);
+
+      if ((!i && j >= firstWeekDay)
+          || (i && dateDay <= lastMonthDay)) {
+        td.textContent = dateDay++;
+      }
+    }
+  }
 }
 
-calendarTable(2019, 10, calendar);
+calendarTable(2001, 1, calendar);
