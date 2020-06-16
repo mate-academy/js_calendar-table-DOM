a99d0fd596c6fcc1208c395d021d0d8677d062b2
nazmeln <nazik100378@gmail.com>
2020-06-16 12:35:49 +0300

    fixed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,54 +2,44 @@
 
 const calendar = document.querySelector('#calendar');
 
-function calendarTable(element, year, month) {
+function calendarTable(year, month, element) {
   // WRITE YOUR CODE HERE
-  const mon = month - 1;
-  const d = new Date(year, mon);
 
-  let table = `<table>
-  <tr><th>MO</th>
-  <th>TU</th>
-  <th>WE</th>
-  <th>TH</th>
-  <th>FR</th>
-  <th>SA</th>
-  <th>SU</th>
-  </tr><tr>`;
+  const monthLength = new Date(year, month, 0).getDate();
+  const monthStartDay = getLocalDay(new Date(year, month - 1));
+  const monthEndDay = getLocalDay(new Date(year, month - 1, monthLength));
+  const daysArr = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'];
+  const datesArr = Array.from(Array(monthLength).keys()).map(date => date + 1);
 
-  for (let i = 0; i < getDay(d); i++) {
-    table += '<td></td>';
-  }
-
-  while (d.getMonth() === mon) {
-    table += '<td>' + d.getDate() + '</td>';
-
-    if (getDay(d) % 7 === 6) {
-      table += '</tr><tr>';
-    }
-
-    d.setDate(d.getDate() + 1);
-  }
-
-  if (getDay(d) !== 0) {
-    for (let i = getDay(d); i < 7; i++) {
-      table += '<td></td>';
-    }
-  }
-
-  table += '</tr></table>';
-
-  element.innerHTML = table;
+  element.innerHTML = `
+  <table>
+    <thead>
+      <tr>    
+     ${daysArr.map(day => `<th>${day}</th>`).join('')}      
+      </tr>
+    </thead>
+    <tbody>
+      <tr>      
+     ${Array(monthStartDay - 1).fill('').map(date => `<td></td>`).join('')}
+     ${datesArr
+    .map(date => (date + monthStartDay - 1) % 7 === 0
+      ? `<td>${date}</td></tr><tr>`
+      : `<td>${date}</td>`)
+    .join('')}
+     ${Array(7 - monthEndDay).fill('').map(date => `<td></td>`).join('')}     
+      </tr>
+    </tbody>
+  </table>`;
 }
 
-function getDay(date) {
+const getLocalDay = date => {
   let day = date.getDay();
 
   if (day === 0) {
     day = 7;
-
-    return day - 1;
   }
-}
 
-calendarTable(calendar, 2020, 6);
+  return day;
+};
+
+calendarTable(2020, 6, calendar);
