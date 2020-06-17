3c07df62cf1be9657eb8630e334533f246b47040
German <germandolia@gmail.com>
2020-06-17 13:37:29 +0300

    fix

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,32 +3,31 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  const weekDays = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  element.innerHTML = `
+  <table>
+    <thead>
+      <tr>
+        <th>Mon</th>
+        <th>Tue</th>
+        <th>Wed</th>
+        <th>Thu</th>
+        <th>Fri</th>
+        <th>Sat</th>
+        <th>Sun</th>
+      </tr>
+    </thead>
+    <tbody>
+    </tbody>
+  </table>
+`;
+
   const firstDay = new Date(year, month - 1, 0).getDay();
   const lastDay = new Date(year, month, 0).getDate();
-  const table = document.createElement('table');
 
-  element.append(table);
-
-  // Table Header
-  const tableHeader = document.createElement('thead');
-
-  table.append(tableHeader);
-
-  for (const weekDayName of weekDays) {
-    const weekDay = document.createElement('th');
-
-    weekDay.innerText = weekDayName;
-    tableHeader.append(weekDay);
-  }
-
-  // Table Body
   const daysInMonth = new Date(year, month, 0).getDate();
   const weeksInMonth = Math.ceil((daysInMonth + firstDay) / 7);
 
-  const tableBody = document.createElement('tbody');
-
-  table.append(tableBody);
+  const tableBody = document.querySelector('tbody');
 
   for (let i = 0; i < weeksInMonth; i++) {
     const week = document.createElement('tr');
@@ -52,4 +51,4 @@ function calendarTable(year, month, element) {
     dayCounter++;
   };
 }
-calendarTable(2020, 11, calendar);
+calendarTable(2020, 2, calendar);
